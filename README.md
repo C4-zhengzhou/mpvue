2bb1a9c5165acd71c00cc56a5a84f919807d9a71
likequincy@outlook.com <jayever@163.com>
2019-01-23 10:52:53 +0800

    update

--- README.md
+++ README.md
@@ -6,28 +6,30 @@
 
 ## 基本用法
 
-``` bash
+```bash
 $ npm install -g @vue/cli @vue/cli-init
 $ vue init C4-zhengzhou/mpvue my-project
 $ cd my-project
-$ npm install
+$ npm i
 $ npm run dev
 ```
 
 ## 主要特性
 
-* 移除 express 等无关依赖
-* 引入 mpvue-entry，移除冗余的 main.js 文件
-* 引入 mpvue-config-loader，支持在 vue 文件中书写页面配置
-* 引入 mpvue-router-patch，支持使用类 vue-router 的 api
-* 引入 vant-weapp，全局配置原生组件库，快速搭建页面
-* 支持官方分包（mpvue-loader@^1.1.0），简化配置方式
-* 引入 less, pug
+- 移除 express 等无关依赖
+- 引入 mpvue-entry，移除冗余的 main.js 文件
+- 引入 mpvue-config-loader，支持在 vue 文件中书写页面配置
+- 引入 mpvue-router-patch，支持使用类 vue-router 的 api
+- 引入 vant-weapp，全局配置原生组件库，快速搭建页面
+- 支持官方分包（mpvue-loader@^1.1.0），简化配置方式
+- 引入 less, 以及 mixin、theme 全局变量自动引入
+- 引入 iconfont, 全局自动引入
+- 引入 mpvue-toast、mpvue-picker、mpvue-animated-number
 
 ## 详细文档请查阅
 
-* [quickstart](http://mpvue.com/mpvue/quickstart)
-* [mpvue-entry](https://github.com/F-loat/mpvue-entry)
-* [mpvue-config-loader](https://github.com/F-loat/mpvue-config-loader)
-* [mpvue-router-patch](https://github.com/F-loat/mpvue-router-patch)
-* [vant-weapp](https://youzan.github.io/vant-weapp)
+- [quickstart](http://mpvue.com/mpvue/quickstart)
+- [mpvue-entry](https://github.com/F-loat/mpvue-entry)
+- [mpvue-config-loader](https://github.com/F-loat/mpvue-config-loader)
+- [mpvue-router-patch](https://github.com/F-loat/mpvue-router-patch)
+- [vant-weapp](https://youzan.github.io/vant-weapp)
--- template/src/pages/index.vue
+++ template/src/pages/index.vue
@@ -1,32 +1,101 @@
 <template lang="pug">
 .void
   .card
-    p this is pug and less page
-  .card
+    p C4-mpvue
+    p 更优雅的起手式
+    p 微信小程序快速开发脚手架
+    p Powered by code4ever.cn
+  .section
     button(@click="bindViewTap") jump to log
+    .info mpvue-router-patch
+  .section
+    button(@click="showToast=true") show toast
+    .info mpvue-toast
+  .section.center
+    AnimatedNumber(:value="animatedNumberData.value" :precision="2")
+    .info @gaomd/mpvue-animated-number
+  .section
+    button(@click="showPicker") show mpvuePicker
+    .info mpvue-picker
+    mpvue-picker(:mode="pickerData.mode" ref="mpvuePicker" :pickerValueDefault="pickerData.pickerValueDefault" @onChange="onChange" @onConfirm="onConfirm" @onCancel="onCancel" :pickerValueArray="pickerData.pickerValueArray")
+  toast(message="hello from toast" :visible.sync="showToast")
+
 </template>
 
 <script>
+import toast from "mpvue-toast";
+import mpvuePicker from "mpvue-picker";
+import AnimatedNumber from "@gaomd/mpvue-animated-number";
 export default {
 	mpType: "page",
-	components: {},
+	components: { toast, mpvuePicker, AnimatedNumber },
 	data() {
-		return {};
+		return {
+			showToast: false,
+			pickerData: {
+				mode: "selector",
+				pickerValueArray: [
+					{
+						label: "住宿费",
+						value: "test1"
+					},
+					{
+						label: "活动费",
+						value: 2
+					},
+					{
+						label: "通讯费",
+						value: 3
+					},
+					{
+						label: "补助",
+						value: 4
+					}
+				],
+				pickerValueDefault: []
+			},
+			animatedNumberData: {
+				value: 2019
+			}
+		};
 	},
 	methods: {
 		bindViewTap() {
 			this.$router.push("/packageA/logs");
+		},
+		showPicker() {
+			this.$refs.mpvuePicker.show();
+		},
+		onConfirm(e) {
+			console.log(e);
+		},
+		onChange(e) {
+			console.log(e);
+		},
+		onCancel(e) {
+			console.log(e);
 		}
 	},
 	created() {}
 };
 </script>
 <style lang="less" scoped>
-.void {
-	.card {
-		padding: 20px;
-		background: @primary;
-		color: @white;
+.card {
+	padding: 50px 20px;
+	background: @primary;
+	color: @white;
+}
+.section {
+  padding: 20px;
+  .border-bottom;
+	.info {
+		.size(12px);
+		.color();
+		text-align: right;
+		&:before {
+			content: "Povide by ";
+			display: inline;
+		}
 	}
 }
 </style>
